91f1a7638f2b655561790adb746530e89bb079f5
iyinraphael <iyinraphael@gmail.com>
2018-11-12 19:07:24 -0800

    tableview implementation in progress

---
+++ New Art Gallery/Model Controller/PaintingModelController.swift
@@ -0,0 +1,9 @@
+//
+//  PaintingModelController.swift
+//  New Art Gallery
+//
+//  Created by Iyin Raphael on 11/12/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import Foundation
