64c4687a17c0d839256eb0965e7c4e427fbe3371
iyinraphael <iyinraphael@gmail.com>
2018-11-12 18:02:01 -0800

    tableView and tableView cell up

---
+++ New Art Gallery/View Controller/PaintingTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaintingTableViewCell.swift
+//  New Art Gallery
+//
+//  Created by Iyin Raphael on 11/12/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class PaintingTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ New Art Gallery/View Controller/PaintingViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  New Art Gallery
+//
+//  Created by Iyin Raphael on 11/12/18.
+//  Copyright © 2018 Iyin Raphael. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var tableView: UITableView!
+    
+
+}
+
